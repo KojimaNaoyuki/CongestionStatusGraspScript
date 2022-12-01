25798a369759eb79ca620d8be68a26d8f526db6c
KojimaNaoyuki <programkoji213@gmail.com>
2022-12-01 23:25:47 +0900

    push

--- regular_shooting_start.sh
+++ regular_shooting_start.sh
@@ -3,4 +3,5 @@
 # 計測を開始する
 curl -X PUT "http://localhost:3001/v1/congestion_data" -d "place_id=$1"
 
-# 定期実行を開始するスクリプトを書く
+# 5分毎に定期実行
+echo "*/5 * * * * ubuntu regular_shooting.sh" > /etc/cron.d/regular_shooting
---
+++ regular_shooting_stop.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# 定期実行を終了
+rm /etc/cron.d/regular_shooting
